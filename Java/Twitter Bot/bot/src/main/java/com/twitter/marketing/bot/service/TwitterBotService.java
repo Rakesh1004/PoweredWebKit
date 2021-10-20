d63ac56da6e3e031c6cdf3e52e9b1343e68a3041
docilefiasco <epshita.hs@gmail.com>
2021-10-20 18:08:36 +0530

    Twitter Bot Java

---
+++ Java/Twitter Bot/bot/src/main/java/com/twitter/marketing/bot/service/TwitterBotService.java
@@ -0,0 +1,8 @@
+package com.twitter.marketing.bot.service;
+
+import java.net.URISyntaxException;
+
+public interface TwitterBotService {
+	
+	public void postTweet() throws URISyntaxException;
+}
---
+++ Java/Twitter Bot/bot/src/main/java/com/twitter/marketing/bot/util/Constants.java
@@ -0,0 +1,112 @@
+package com.twitter.marketing.bot.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Constants {
+
+	public static final String POST_TWEET_URL = "https://api.twitter.com/1.1/statuses/update.json";
+	
+	public static final String AUTHORIZATION = "Authorization";
+	
+	public static final String STATUS = "status";
+	
+	public static final Integer MAX_TWEET_LENGTH = 280;
+	
+	public static final List<String> commonHashtagsList = Arrays.asList(
+			"#100DaysOfCode",
+			"#100DaysOfCloud",
+			"#100DaysOfMLCode",
+			"#java",
+			"#coder",
+			"#CodeNewbie",
+			"#developer",
+			"#javascript",
+			"#IOT",
+			"#serverless",
+			"#programming",
+			"#coding",
+			"#cloud",
+			"#code",
+			"#IoT",
+			"#AI",
+			"#ArtificialIntelligence",
+			"#InternetOfThings",
+			"#python",
+			"#technology",
+			"#opensource",
+			"#blockhain",
+			"#bootcamp",
+			"#computer",
+			"#programmingmemes",
+			"#tech",
+			"#css",
+			"#html",
+			"#softwaredeveloper",
+			"#webdeveloper",
+			"#dataanalytics",
+			"#datascientist",
+			"#backend",
+			"#frontend",
+			"#fullstack",
+			"#bitcoin",
+			"#crypto",
+			"#learning",
+			"#promo",
+			"#free",
+			"#javascript30",
+			"#codinglife",
+			"#linux",
+			"#cybersecurity",
+			"#data",
+			"#learnprogramming",
+			"#machinelearning",
+			"#pythoncode",
+			"#pythonprogramming",
+			"#computerscience",
+			"#javascript30"
+			);
+	
+	public static final List<String> courseList = Arrays.asList(
+			"https://www.udemy.com/course/build-a-google-assistant-app-using-java-and-spring-boot/?couponCode=C0UP0N",
+			"https://www.udemy.com/course/learn-how-to-create-a-spring-boot-restful-java-application/?couponCode=C0UP0N"
+			);
+	
+	public static final List<String> courseMessagesList = Arrays.asList(
+			"Anyone who stops learning is old, whether at twenty or eighty.",
+			"An investment in knowledge pays the best interest.",
+			"The only person who is educated is the one who has learned how to learn and change.",
+			"Technology matters",
+			"Check my programming course",
+			"Become a software developer",
+			"Learning never exhausts the mind.",
+			"Become a web developer",
+			"Limited promo available",
+			"Last chance to sign up",
+			"The great aim of education is not knowledge but action.",
+			"Full course available",
+			"Skill up",
+			"Smooth seas do not make skillful sailors.",
+			"Learn more and evolve",
+			"Education without application is just entertainment.",
+			"No problem can withstand the assault of sustained thinking.",
+			"Trends in 2021",
+			"Grow your career path",
+			"Become a better programmer",
+			"Software development trends",
+			"Learning is a treasure that will follow its owner everywhere.",
+			"You are in control of your future",
+			"Never give up",
+			"Learn...and grow",
+			"Being a student is easy. Learning requires actual work.",
+			"Work hard",
+			"Play hard",
+			"Be strong",
+			"Learn something everyday",
+			"Become a better version",
+			"You're so much stronger than your excuses",
+			"Don't Quit",
+			"Hold the vision, trust the process",
+			"Some people want it to happen, some wish it would happen, others make it happen"
+			);
+}
