cdde5951d1221e32983b4a0e90f471457a151b6b
Apurva Jyoti Paul <apurva721034@gmail.com>
2021-10-24 01:03:07 +0530

    Added blog text model

--- Django/BlogWebsite/blogapp/blogs/models.py
+++ Django/BlogWebsite/blogapp/blogs/models.py
@@ -7,3 +7,7 @@ class userb(models.Model):
     name=models.CharField(max_length=1000)
     username=models.CharField(max_length=100)
     doc=models.DateTimeField(auto_now_add=True)
+
+class blogs(models.model):
+    user=models.ForeignKey(userb,on_delete=models.CASCADE)
+    text=models.CharField(max_length=10000)
