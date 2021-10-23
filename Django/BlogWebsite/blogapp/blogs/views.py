da76a22036b380933d84422f89fe0abb435572bd
Apurva Jyoti Paul <apurva721034@gmail.com>
2021-10-23 21:46:40 +0530

    Added Basic views to blogweb app in django

--- Django/BlogWebsite/blogapp/blogs/views.py
+++ Django/BlogWebsite/blogapp/blogs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import userb
+
 
 # Create your views here.
+
+
+def show(request):
+    usr=userb.objects.all()
+    return render(request,'users.html',{'user':usr}) 
